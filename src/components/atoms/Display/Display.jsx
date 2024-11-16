5c92878a7ba928c20f5da0e84a62173be4d33012
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-11-16 20:37:16 +0100

    Add the Display component and include it in the Calculator component

---
+++ src/components/atoms/Display/Display.jsx
@@ -0,0 +1,7 @@
+import './Display.css'
+
+export const Display = () => (
+  <div>
+    <h2 id="display">0</h2>
+  </div>
+);
--- src/components/molecules/Calculator/Calculator.jsx
+++ src/components/molecules/Calculator/Calculator.jsx
@@ -1,6 +1,8 @@
 import './Calculator.css'
+import { Display } from '../../atoms/Display'
 
 export const Calculator = () => (
   <section id="calculator">
+    <Display />
   </section>
 );
