185d7934eae194edb5d510fd7672cd5a368b2a1c
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-11-17 18:15:58 +0100

    Rename 'operator' buuttons

--- src/components/molecules/Buttons/Buttons.jsx
+++ src/components/molecules/Buttons/Buttons.jsx
@@ -5,20 +5,20 @@ export const Buttons = ({ handleButtonClicked }) => (
   <div id="buttons">
     <Button className="specialOperation" content="&#8730;" handleButtonClicked={handleButtonClicked} />
     <Button className="specialOperation" content="&sup2;" handleButtonClicked={handleButtonClicked} />
-    <Button className="operation" content="&#94;" handleButtonClicked={handleButtonClicked} />
-    <Button className="operation" content="/" handleButtonClicked={handleButtonClicked} />
+    <Button className="operator" content="&#94;" handleButtonClicked={handleButtonClicked} />
+    <Button className="operator" content="/" handleButtonClicked={handleButtonClicked} />
     <Button className="number" content="7" handleButtonClicked={handleButtonClicked} />
     <Button className="number" content="8" handleButtonClicked={handleButtonClicked} />
     <Button className="number" content="9" handleButtonClicked={handleButtonClicked} />
-    <Button className="operation" content="*" handleButtonClicked={handleButtonClicked} />
+    <Button className="operator" content="*" handleButtonClicked={handleButtonClicked} />
     <Button className="number" content="4" handleButtonClicked={handleButtonClicked} />
     <Button className="number" content="5" handleButtonClicked={handleButtonClicked} />
     <Button className="number" content="6" handleButtonClicked={handleButtonClicked} />
-    <Button className="operation" content="-" handleButtonClicked={handleButtonClicked} />
+    <Button className="operator" content="-" handleButtonClicked={handleButtonClicked} />
     <Button className="number" content="1" handleButtonClicked={handleButtonClicked} />
     <Button className="number" content="2" handleButtonClicked={handleButtonClicked} />
     <Button className="number" content="3" handleButtonClicked={handleButtonClicked} />
-    <Button className="operation" content="+" handleButtonClicked={handleButtonClicked} />
+    <Button className="operator" content="+" handleButtonClicked={handleButtonClicked} />
     <Button id="clear" content="C" handleButtonClicked={handleButtonClicked} />
     <Button className="number" content="0" handleButtonClicked={handleButtonClicked} />
     <Button id="delete">
