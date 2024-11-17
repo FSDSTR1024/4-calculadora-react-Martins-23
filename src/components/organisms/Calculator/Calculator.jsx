aaad5a371506ff731d6535b42c62a7f26535d5d1
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-11-17 18:12:59 +0100

    Add some explanatory comments

--- src/components/organisms/Calculator/Calculator.jsx
+++ src/components/organisms/Calculator/Calculator.jsx
@@ -5,11 +5,15 @@ import { Display } from "../../atoms/Display"
 import { Buttons } from "../../molecules/Buttons"
 
 export const Calculator = () => {
+  // List of numbers inside the operation
   const [numbers, setNumbers] = useState(['0']);
+  // Flag to check if the last button clicked was an operator
   const [wasOperatorClicked, setWasOperatorClicked] = useState(false);
 
+  // String to display the operation
   const [displayStr, setdisplayStr] = useState('0');
 
+  // UseEffect to update the displayStr when numbers list changes
   useEffect(() => {
     let newDisplayStr = '';
     numbers.forEach((number, index) => {
@@ -18,6 +22,7 @@ export const Calculator = () => {
     setdisplayStr(newDisplayStr);
 }, [numbers]);
 
+  // Method to handle when a number is clicked, to update the numbers list
   const handleNumberClicked = (number) => {
     if (wasOperatorClicked) setNumbers([...numbers, number]);
     else {
@@ -28,6 +33,7 @@ export const Calculator = () => {
     setWasOperatorClicked(false);
   }
 
+  // Method to handle when a button is clicked, to update the operation and calculator's lists
   const handleButtonClicked = (event) => {
     const buttonClass = event.target.className;
     if (buttonClass.includes("number")) handleNumberClicked(event.target.textContent);
